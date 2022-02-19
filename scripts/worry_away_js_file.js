20133647e8a996cf0e6db8f707872ad2b2c3f045
MrMustard-79 <kristofferjonhendricks@gmail.com>
2022-02-19 14:42:25 -0500

    Fixed?

--- scripts/worry_away_js_file.js
+++ scripts/worry_away_js_file.js
@@ -1,11 +1,23 @@
 var input = document.getElementById("venttext");
 
-input.addEventListener("keyup", function(en)
-{
-    if(en.keyCode === 13)
-    {
-        en.preventDefault();
-        document.getElementById("ventBtn").click();
-    }
-})
+// input.addEventListener("keyup", function(en)
+// {
+//     if(en.keyCode === 13)
+//     {
+//         en.preventDefault();
+//         document.getElementById("ventBtn").click();
+//     }
+// })
 
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.hight = window.innerHeight;
+
+
+c.beginPath();
+c.fillStyle = "#FFFFFF";
+c.fillRect(5,5,5,5);
+c.fillRect(150,5,5,5,);
+c.closePath();
